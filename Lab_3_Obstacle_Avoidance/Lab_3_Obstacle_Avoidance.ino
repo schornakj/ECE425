9ecd2396c9d2d12b7b14234926a0296dbeaac53d
schornakj <joe.schornak@gmail.com>
2015-12-10 01:22:42 -0500

    Added Lab 3 Arduino sketch.

---
+++ Lab_3_Obstacle_Avoidance/Lab_3_Obstacle_Avoidance.ino
@@ -0,0 +1,12 @@
+// Copy the actual code over to here ASAP!
+// See if commiting changes to this shared version works as expected.
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
