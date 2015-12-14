83162fd25bd4261faee6266f15617ea9f6b50dba
schornakj <joe.schornak@gmail.com>
2015-12-13 22:10:58 -0500

    Added obstacle avoidance
    - Broke the state machine (fix this please!)
    - Implemented a potential field function that generates a magnitude and
    angle pointing away from all obstacles around the robot.
    - Added WanderAvoid to drive around while avoiding obstacles.

--- Lab_3_Obstacle_Avoidance/Lab_3_Obstacle_Avoidance.ino
+++ Lab_3_Obstacle_Avoidance/Lab_3_Obstacle_Avoidance.ino
@@ -1,5 +1,3 @@
-// Include libraries
-#include <IRremote.h>
 #include <ArduinoRobot.h>
 #include <math.h>
 #include <Wire.h>
@@ -23,34 +21,41 @@
 #define STATE_WANDER_AVOID 6
 #define STATE_WANDER_AVOID_OBSTACLE 7
 
-int RECV_PIN = TKD1; // the pin the IR receiver is connected to
-IRrecv irrecv(RECV_PIN); // an instance of the IR receiver object
-decode_results results; // container for received IR codes
+#define FORWARD 0
+#define RIGHT 1
+#define BACK 2
+#define LEFT 3
 
-long frontSensorValue;
-long leftSensorValue;
-long rightSensorValue;
-long backSensorValue;
+float frontSensorValue;
+float leftSensorValue;
+float rightSensorValue;
+float backSensorValue;
 int frontSensorPin = TKD1;
 int leftSensorPin = TK4;
 int rightSensorPin = TK0;
 int backSensorPin = TK6;
 int ftSonarPin = TKD1;
 int wanderCount = 0;
-int state = STATE_AGGRESSIVE_KID_IDLE;
 
+int state = STATE_WANDER_AVOID;
+
+
+int defaultMotorSpeed = 200;
 int leftMotorSpeed = 0;
 int rightMotorSpeed = 0;
-int defaultMotorSpeed = 200;
 
+float spinDegreesPerMilisecond = (168 / 90) * 0.325;
 
 int sensorCount = 0;
-int frontSensorDistance;
-int leftSensorDistance;
-int rightSensorDistance;
-int backSensorDistance;
+float frontSensorDistance = -1;
+int leftSensorDistance = -1;
+int rightSensorDistance = -1;
+int backSensorDistance = -1;
 int obstacleDistanceThreshold = 20;
 int obstacleDistanceFarThreshold = 50;
+
+int potentialFieldAngle = 0;
+int potentialFieldMagnitude = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -59,82 +64,68 @@ void setup() {
   Robot.beginSD();
   Serial.begin(9600);
 
-  irrecv.enableIRIn(); // Start the receiver
+  //irrecv.enableIRIn(); // Start the receiver
 
-  Robot.stroke(0, 0, 0);
-  Robot.fill(255, 255, 255);
-  Robot.text("1: shy kid", 5, 10);
-  Robot.text("2: aggressive kid", 5, 20);
-  Robot.text("3: random wander", 5, 30);
-  Robot.text("4: wander and avoid", 5, 40);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //  // read analog sensor
-  sensorCount = sensorCount + 1;
-  if (sensorCount == 1) {
-    leftSensorValue = Robot.analogRead(leftSensorPin);
-    //leftSensorValue = (Robot.analogRead(leftSensorPin) + leftSensorValue) / 2;
-    //leftSensorValue = (Robot.analogRead(leftSensorPin) + leftSensorValue) / 3;
-    leftSensorDistance = 14235 * pow(leftSensorValue, -1.167);
-    Robot.text("left sensor reading", 3, 80);
-    Robot.debugPrint(leftSensorDistance, 5, 90);
+  Robot.stroke(0, 0, 0);
+  Robot.text("left sensor reading", 3, 80);
+  Robot.text("right sensor reading", 3, 100);
+  Robot.text("back sensor reading", 3, 120);
+  Robot.text("front sensor reading", 5, 60);
+
+  Robot.fill(255, 255, 255);
+  Robot.stroke(255, 255, 255);
+
+
+  ////read digital sensor
+  pinMode(ftSonarPin, OUTPUT);//set the PING pin as an output
+  Robot.digitalWrite(ftSonarPin, LOW); //set the PING pin low first
+  delayMicroseconds(2);//wait 2 us
+  Robot.digitalWrite(ftSonarPin, HIGH);//trigger sonar by a 2 us HIGH PULSE
+  delayMicroseconds(5);//wait 5 us
+  Robot.digitalWrite(ftSonarPin, LOW);//set pin low first again
+  pinMode(ftSonarPin, INPUT);//set pin as input with duration as reception time
+  frontSensorValue = pulseIn(ftSonarPin, HIGH); //measures how long the pin is high
+  float frontSensorDistanceTemp = 0.0236 * frontSensorValue + 0.2049;
+  if (((int)frontSensorDistanceTemp > 0) && ((int)frontSensorDistanceTemp < 1000)) {
+    frontSensorDistance = frontSensorDistanceTemp;
   }
-  else if (sensorCount == 2) {
-    rightSensorValue = Robot.analogRead(rightSensorPin);
-    //rightSensorValue = (Robot.analogRead(rightSensorPin) + rightSensorValue) / 2;
-    //rightSensorValue = (Robot.analogRead(rightSensorPin) + rightSensorValue) / 3;
-    rightSensorDistance = 14235 * pow(rightSensorValue, -1.167);
-    Robot.text("right sensor reading", 3, 100);
-    Robot.debugPrint(rightSensorDistance, 5, 110);
-  }
-  else if (sensorCount == 3) {
-    backSensorValue = Robot.analogRead(backSensorPin);
-    //backSensorValue = (Robot.analogRead(backSensorPin) + backSensorValue) / 2;
-    //backSensorValue = (Robot.analogRead(backSensorPin) + backSensorValue) / 3;
-    backSensorDistance = 14235 * pow(backSensorValue, -1.167);
-    Robot.text("back sensor reading", 3, 120);
-    Robot.debugPrint(backSensorDistance, 5, 130);
-  }
-  else if (sensorCount == 4) {
-    ////read digital sensor
-    pinMode(ftSonarPin, OUTPUT);//set the PING pin as an output
-    Robot.digitalWrite(ftSonarPin, LOW); //set the PING pin low first
-    delayMicroseconds(2);//wait 2 us
-    Robot.digitalWrite(ftSonarPin, HIGH);//trigger sonar by a 2 us HIGH PULSE
-    delayMicroseconds(5);//wait 5 us
-    Robot.digitalWrite(ftSonarPin, LOW);//set pin low first again
-    pinMode(ftSonarPin, INPUT);//set pin as input with duration as reception time
-    frontSensorValue = pulseIn(ftSonarPin, HIGH); //measures how long the pin is high
-    frontSensorDistance = 0.0236 * frontSensorValue + 0.2049;
-    Robot.text("front sensor reading", 5, 60);
-    Robot.debugPrint(frontSensorDistance, 5, 70);
-    sensorCount = 0;
+
+  Robot.rect(0, 70, 100, 10);
+
+  Robot.debugPrint(frontSensorDistance, 5, 70);
+
+  leftSensorDistance = IRSensorLinearize(Robot.analogRead(leftSensorPin));
+  Robot.rect(0, 90, 100, 10);
+  Robot.debugPrint(leftSensorDistance, 5, 90);
+
+  rightSensorDistance = IRSensorLinearize(Robot.analogRead(rightSensorPin));
+  Robot.rect(0, 110, 100, 10);
+  Robot.debugPrint(rightSensorDistance, 5, 110);
+
+  backSensorDistance = IRSensorLinearize(Robot.analogRead(backSensorPin));
+  Robot.rect(0, 130, 100, 10);
+  Robot.debugPrint(backSensorDistance, 5, 130);
+
+  potentialFieldMagnitude = sqrt(pow(leftSensorDistance - rightSensorDistance, 2) + pow(frontSensorDistance - backSensorDistance, 2));
+  potentialFieldAngle = atan2(leftSensorDistance - rightSensorDistance, frontSensorDistance - backSensorDistance) * 360 / (2 * PI);
+
+  Robot.rect(0, 140, 100, 10);
+  Robot.debugPrint(potentialFieldMagnitude, 5, 140);
+  Robot.debugPrint(potentialFieldAngle, 50, 140);
+  Serial.print("Distance: ");
+  Serial.println(potentialFieldMagnitude);
+  Serial.print("Angle: ");
+  Serial.println(potentialFieldAngle);
+  
+  if (Robot.keyboardRead() == BUTTON_MIDDLE) {
+
   }
 
 
-
-  if (irrecv.decode(&results)) {
-    processResult(); // if there is an IR command, process it
-    irrecv.resume(); // resume receiver
-  }
-
-  // close obstacle
-  /*
-    if (frontSensorValue < obstacleDistance) {
-    if (state == STATE_SHY_KID) {
-      state = STATE_SHY_KID_OBSTACLE;
-    }
-    else if (state == STATE_AGGRESSIVE_KID) {
-      state = STATE_AGGRESSIVE_KID_OBSTACLE;
-    }
-    else if (state == STATE_WANDER_AVOID) {
-      state = STATE_WANDER_AVOID_OBSTACLE;
-    }
-    }
-  */
-  //else {
   if (state == STATE_SHY_KID) {
     ShyKid();
   }
@@ -146,20 +137,13 @@ void loop() {
   }
   else if (state == STATE_AGGRESSIVE_KID_CHARGE) {
     AggressiveKidCharge();
-  }
-  else if (state == STATE_RANDOM_WANDER) {
-    RandomWander();
-  }
-  else if (state == STATE_WANDER_AVOID) {
+  } else if (state == STATE_WANDER_AVOID) {
     WanderAvoid();
   }
-  else if (state == STATE_WANDER_AVOID_OBSTACLE) {
-  }
-  //}
 
-  Robot.motorsWrite(rightMotorSpeed, leftMotorSpeed);
 }
-void processResult() {
+/*
+  void processResult() {
   unsigned long res = results.value;
   // print the value to the screen
   //Robot.debugPrint(res, 5, 15);
@@ -229,8 +213,8 @@ void processResult() {
       DisplayMenu();
       break;
   }
-}
-
+  }
+*/
 
 void DisplayMenu() {
   Robot.stroke(0, 0, 0);
@@ -289,30 +273,50 @@ void RandomWander() {
     //Robot.motorsWrite(rightMotorSpeed, leftMotorSpeed);
   }
 }
+void DriveInputAngle(int inputAngle) {
+  if (inputAngle > 0) {
+    rightMotorSpeed = defaultMotorSpeed;
+    leftMotorSpeed = -defaultMotorSpeed;
+    delay(abs(inputAngle) / spinDegreesPerMilisecond);
+    //Robot.motorsWrite(0, 0);
+  } else {
+    rightMotorSpeed = -defaultMotorSpeed;
+    leftMotorSpeed = defaultMotorSpeed;
+    delay(abs(inputAngle) / spinDegreesPerMilisecond);
+    //Robot.motorsWrite(0, 0);
+  }
+}
 
 void WanderAvoid() {
-  wanderCount++;
-  if (wanderCount > 10) {
-    wanderCount = 0;
-    leftMotorSpeed = random(75, 225);
-    rightMotorSpeed = random(75, 225);
-    //Robot.motorsWrite(rightMotorSpeed, leftMotorSpeed);
-  }
-}
+  // If there is an obstacle too close, take evasive action.
+  if (potentialFieldMagnitude < 75) {
+    if (potentialFieldAngle > 0) {
 
+      Robot.motorsWrite(defaultMotorSpeed, -defaultMotorSpeed);
+      delay(abs(potentialFieldAngle) / spinDegreesPerMilisecond);
+      Robot.motorsWrite(0, 0);
+    } else {
+      Robot.motorsWrite(-defaultMotorSpeed, defaultMotorSpeed);
+      delay(abs(potentialFieldAngle) / spinDegreesPerMilisecond);
+      //Robot.motorsWrite(0, 0);
+    }
 
-bool LeftObstacleClose() {
-  if (leftSensorDistance < obstacleDistanceThreshold) {
-    return true;
+    delay(250);
   }
-  return false;
-}
 
-bool RightObstacleClose() {
-  if (rightSensorDistance < obstacleDistanceThreshold) {
-    return true;
+  // Pick motor speeds for random value.
+  int leftValue = random(75, 200);
+  int rightValue = random(75, 200);
+
+  // Maximize the overall robot speed.
+  if (rightValue < leftValue){
+    leftValue = 200;
+  } else {
+    rightValue = 200;
   }
-  return false;
+  Robot.motorsWrite(rightValue, leftValue);
+  // Wait a while
+  delay(250);
 }
 
 bool FrontObstacleClose() {
@@ -322,12 +326,11 @@ bool FrontObstacleClose() {
   return false;
 }
 
-bool BackObstacleClose() {
-  if (backSensorDistance < obstacleDistanceThreshold) {
-    return true;
-  }
-  return false;
+float IRSensorLinearize(int input) {
+  return 14235 * pow(input, -1.167);
 }
+
+
 
 void BrakeMotors() {
   if (leftMotorSpeed != 0 || rightMotorSpeed != 0) {
